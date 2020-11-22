e35afda7c7b737639bdca26c24cab3a835b3f0e9
z <ai977313677@163.com>
2020-11-22 23:48:51 +0800

    Week08

--- Week_08/README.md
+++ Week_08/README.md
@@ -71,6 +71,34 @@ const quickSort = (nums, left, right) => {
 }
 
 // 归并排序
+const mergeSort = arr => {  // 采用自上而下的递归方法
+    var len = arr.length;
+    if (len < 2) {
+        return arr;
+    }
+    var middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
 
-// 
+const merge = (left, right) => {
+    var result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
+}
 ```
--- Week_08/test.js
+++ Week_08/test.js
@@ -70,4 +70,35 @@ const quickSortDemo = nums => {
     return nums;
 }
 
-console.log(quickSortDemo([2, 5, 1, 512, 324, 3465, 3423, 4, 123, 12, 3, 1254, 32, 5, 34]));
+const mergeSort = arr => {  // 采用自上而下的递归方法
+    var len = arr.length;
+    if (len < 2) {
+        return arr;
+    }
+    var middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+    var result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
+}
+
+console.log(mergeSort([2, 5, 1, 512, 324, 3465, 3423, 4, 123, 12, 3, 1254, 32, 5, 34]));
