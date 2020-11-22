d5af41eea14d5435f4c07381b30fee4850688867
z <ai977313677@163.com>
2020-11-23 00:12:40 +0800

    week08

--- Week_08/LRUCache.js
+++ Week_08/LRUCache.js
@@ -2,7 +2,8 @@
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-
+    this.capacity = capacity;
+    this.cache = [];
 };
 
 /** 
@@ -10,7 +11,17 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-
+    if (this.cache.length === 0) { return -1; }
+    for (let i = 0; i < this.cache.length; i++) {
+        if (this.cache[i].key === key) {
+            const value = this.cache[i].value;
+            delete this.cache[i];
+            this.cache = this.cache.filter(item => item);
+            this.cache.unshift({ key: key, value: value });
+            return value;
+        }
+    }
+    return -1;
 };
 
 /** 
@@ -19,7 +30,17 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-
+    const idx = this.get(key);
+    if (idx > 0) {
+        this.cache[0].value = value;
+    } else {
+        if (this.cache.length >= this.capacity) {
+            this.cache.length = this.capacity - 1;
+            this.cache.unshift({ key: key, value: value });
+        } else {
+            this.cache.unshift({ key: key, value: value });
+        }
+    }
 };
 
 /**
